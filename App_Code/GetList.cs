0f05ce00e62e7d627d8531192396a23ed78a8739
Arvin Jay De Vera <ajdevera@pjlhuillier.com>
2024-06-07 16:40:16 +0800

    TRY

--- App_Code/GetList.cs
+++ App_Code/GetList.cs
@@ -17,11 +17,6 @@ using System.Web.UI.WebControls;
 
 public class GetList
 {
-
-
-
-
- //SOMETHING TEST
     AuditTrail auditTrail = new AuditTrail();
     public IList<String> GetPartnerList()
     {
